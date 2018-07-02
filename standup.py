5958c20be0b21d0778bd6dfaaacc8f09d34a107b
Craig Vitter <craig.vitter@gmail.com>
2018-07-02 16:50:53 -0400

    Refactored code to simplify

--- standup.py
+++ standup.py
@@ -3,21 +3,15 @@ from flask import request
 import json
 from datetime import datetime
 
-__doc__ = """\
-standup.py
-
-------------------------------------------------------------------------------------------
-Flask application below
-"""
 
 app = Flask(__name__)
- 
-@app.route( "/standup", methods = [ 'POST' ] )
-def slashCommand():
-    
+
+
+@app.route("/standup", methods=['POST'])
+def slash_command():
     """
-    Retrieve text field from the form request which contains the message entered by the
-    user who invoked the slash command
+    Retrieve text field from the form request which contains
+    the message entered by the user who invoked the slash command
     """
     form_text = request.form["text"]
     if len(form_text) > 0:
@@ -29,42 +23,36 @@ def slashCommand():
                 form_text,
                 datetime.strftime(datetime.now(), "%Y%m%d"),
             )
-            
+
         """
         Create data json object to return to Mattermost with
-            response_type = in_channel (everyone sees) or ephemeral (only sender sees)
-            text = the message to send
+        response_type = in_channel (everyone sees) or ephemeral (only sender sees)
+        text = the message to send
         """
         data = {
             "response_type": "in_channel",
             "text": output,
         }
-        
-        """
-        Create the response object to send to Mattermost with the
-        data object written as json, 200 status, and proper mimetype
-        """
-        response = app.response_class(
-            response = json.dumps(data),
-            status = 200,
-            mimetype = 'application/json'
-        )
     else:
         """
-        If the user didn't type a message send a note that only they see about typing a message
+        If the user didn't type a message send a note that only
+        they see about typing a message
         """
         data = {
             "response_type": "ephemeral",
             "text": "Error: No status message entered. Please try again.",
         }
-        
-        response = app.response_class(
-            response = json.dumps(data),
-            status = 200,
-            mimetype = 'application/json'
-        )
 
+    """
+    Create the response object to send to Mattermost with the
+    data object written as json, 200 status, and proper mimetype
+    """
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
     return response
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5004, debug = False)
+    app.run(host='0.0.0.0', port=5004, debug=False)
