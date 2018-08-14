929b8313fa6283cfe50fd52716bf6ea70e8a79c4
Ubuntu <ubuntu@ip-10-10-1-211.us-west-1.compute.internal>
2018-08-14 20:56:08 +0000

    first pass

--- standup.py
+++ standup.py
@@ -2,6 +2,21 @@ from flask import Flask
 from flask import request
 import json
 from datetime import datetime
+from ConfigParser import SafeConfigParser
+
+
+"""
+Read in and parse config options
+"""
+
+config = SafeConfigParser()
+config.read('config')
+
+listen_port = config.get('main', 'listen_port')
+debug_mode = config.get('main', 'debug_mode')
+include_channel = config.get('main', 'include_channel')
+date_format = config.get('main', 'tag_date_format')
+compact_date= config.get('main', 'compact_date')
 
 
 app = Flask(__name__)
@@ -14,15 +29,20 @@ def slash_command():
     the message entered by the user who invoked the slash command
     """
     form_text = request.form["text"]
-    channel = request.form["channel_name"]
+    channel = "_"+request.form["channel_name"] if include_channel == 'True' else " "
+    short_tag_date = "%m%d%Y" if date_format == 'mdy' else "%Y%m%d"
+    long_tag_date =  "%m_%d_%Y" if date_format == 'mdy' else "%Y_%m_%d"
+    tag_date = short_tag_date if compact_date == 'True' else long_tag_date
+
+
     if len(form_text) > 0:
         """
         Format the return message with markdown with standup hash tags
         """
-        output = "##### Status Update for {}\n\n{}\n\n#standup-{} #standup".format(
+	output = "##### Status Update for {}\n\n{}\n\n#standup-{} #standup".format(
                 datetime.strftime(datetime.now(), "%A %-d %B %Y"),
                 form_text,
-                datetime.strftime(datetime.now(), "%m_%d_%Y"+"_"+channel),
+                datetime.strftime(datetime.now(), tag_date+channel),
             )
 
         """
@@ -55,5 +75,6 @@ def slash_command():
     )
     return response
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5004, debug=False)
+    app.run(host='0.0.0.0', port=listen_port, debug=debug_mode)
